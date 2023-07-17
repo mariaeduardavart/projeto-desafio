d760cb6f101a6013d95f5bb1b6e431c10afc8804
mariaeduardavart <dudavart@gmail.com>
2023-07-17 17:38:15 -0300

    Adição dos serviços TransferenciaService e ContaService

---
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -0,0 +1,58 @@
+package br.com.banco.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.banco.entity.Transferencia;
+import br.com.banco.repository.TransferenciaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class TransferenciaService {
+
+    @Autowired
+    private TransferenciaRepository transferenciaRepository;
+
+    public List<Transferencia> obterTodasTransferencias() {
+        return transferenciaRepository.findAll();
+    }
+
+    public List<Transferencia> obterTransferenciasPorPeriodoTempo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+        List<Transferencia> transferencias = transferenciaRepository.findByDataTransferenciaBetween(dataInicial, dataFinal);
+    
+        if (transferencias.isEmpty()) {
+            throw new NoSuchElementException("Não foram encontradas transferências no período informado.");
+        }
+        
+        return transferencias;
+    }
+
+    public List<Transferencia> obterTransferenciasPorNomeOperador(String nomeOperador) {
+        List<Transferencia> transferencias = transferenciaRepository.findByNomeOperadorTransacao(nomeOperador);
+    
+        if (transferencias.isEmpty()) {
+            throw new NoSuchElementException("Não foram encontradas transferências para o operador: " + nomeOperador);
+        }
+        
+        return transferencias;
+    }
+
+    public Transferencia incluirTransferencia(Transferencia transferencia) {
+        return transferenciaRepository.saveAndFlush(transferencia);
+    }
+
+    public void excluirTransferencia(Long id) {
+        Optional<Transferencia> transferenciaOptional = transferenciaRepository.findById(id);
+        if (transferenciaOptional.isPresent()) {
+            Transferencia transferencia = transferenciaOptional.get();
+            transferenciaRepository.delete(transferencia);
+        } else {
+            throw new NoSuchElementException("Transferência não encontrada com o ID: " + id);
+        }
+
+    }
+}
